eea6eab94ffacba15ccec8ace29e0548e8a6fc99
MESC2004 <a01028033@tec.mx>
2024-08-13 16:41:26 -0600

    dynamic data showing for small screen sizes

--- WebDev/test-admin/src/App.tsx
+++ WebDev/test-admin/src/App.tsx
@@ -1,13 +1,13 @@
 import {
   Admin,
-  Resource,
-  ListGuesser,
-  EditGuesser,
-  ShowGuesser,
+  Resource
 } from "react-admin";
 import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
+import { UserList } from "./users";
 
 export const App = () => (
-  <Admin layout={Layout} dataProvider={dataProvider}></Admin>
+  <Admin layout={Layout} dataProvider={dataProvider}>
+    <Resource name="users" list = {UserList}/>
+  </Admin>
 );
