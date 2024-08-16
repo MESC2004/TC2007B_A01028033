ffd65e83857166763a77806b9378d7fa47e1fa5c
MESC2004 <a01028033@tec.mx>
2024-08-16 16:44:02 -0600

    added mui elements and created dashboard.tsx

--- WebDev/test-admin/src/App.tsx
+++ WebDev/test-admin/src/App.tsx
@@ -3,16 +3,22 @@ import {
   Resource,
   ListGuesser,
   ShowGuesser,
-  EditGuesser
+  EditGuesser,
 } from "react-admin";
+
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
+import {Dashboard} from './Dashboard';
 import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
 import { UserList, UserEdit, UserCreate } from "./users";
 import { PostList, PostEdit, PostCreate } from "./posts";
 
 export const App = () => (
-  <Admin layout={Layout} dataProvider={dataProvider}>
-    <Resource name="users" list = {UserList} edit = {UserEdit} create = {UserCreate}/>
-    <Resource name="posts" list = {PostList} edit = {PostEdit} create = {PostCreate}/>
+  <Admin layout={Layout} dataProvider={dataProvider} dashboard={Dashboard}>
+    <Resource name="users" list = {UserList} edit = {UserEdit} create = {UserCreate} icon={UserIcon}/>
+    <Resource name="posts" list = {PostList} edit = {PostEdit} create = {PostCreate} icon={PostIcon}/>
+
   </Admin>
 );
+
