5d69330fc928a58ee5f4a26da98fb2ccdfff94f0
MESC2004 <a01028033@tec.mx>
2024-08-16 18:51:53 -0600

    added CRUD for albums with the appropriate relations to users

--- WebDev/test-admin/src/App.tsx
+++ WebDev/test-admin/src/App.tsx
@@ -9,6 +9,7 @@ import {
 import PostIcon from "@mui/icons-material/Book";
 import UserIcon from "@mui/icons-material/Group";
 import CommentIcon from "@mui/icons-material/Comment"
+import CollectionsIcon from "@mui/icons-material/Collections";
 import {Dashboard} from './Dashboard';
 import { authProvider } from './authProvider';
 import { Layout } from "./Layout";
@@ -16,12 +17,14 @@ import { dataProvider } from "./dataProvider";
 import { UserList, UserEdit, UserCreate } from "./users";
 import { PostList, PostEdit, PostCreate } from "./posts";
 import { CommentList, CommentEdit, CommentCreate} from "./comments";
+import { AlbumList, AlbumEdit, AlbumCreate } from "./albums"
 
 export const App = () => (
   <Admin layout={Layout} authProvider={authProvider} dataProvider={dataProvider} dashboard={Dashboard}>
     <Resource name="users" list = {UserList} edit = {UserEdit} create = {UserCreate} icon={UserIcon}/>
     <Resource name="posts" list = {PostList} edit = {PostEdit} create = {PostCreate} icon={PostIcon}/>
     <Resource name="comments" list = {CommentList} edit = {CommentEdit} create = {CommentCreate} icon={CommentIcon} />
+    <Resource name="albums" list = {AlbumList} edit = {AlbumEdit} create = {AlbumCreate} icon={CollectionsIcon}/>
 
   </Admin>
 );
--- WebDev/test-admin/src/albums.tsx
+++ WebDev/test-admin/src/albums.tsx
@@ -10,35 +10,38 @@ import {
     TextInput,
     Create
 } from "react-admin";
-export const PostList = () => (
+
+export const AlbumList = () => (
     <List>
         <Datagrid>
             <TextField source="id" />
             <ReferenceField source="userId" reference="users" />
             <TextField source="title" />
-            <TextField source="body" />
             <EditButton/>
         </Datagrid>
     </List>
 );
 
-export const PostEdit = () => (
+
+export const AlbumEdit = () => (
     <Edit>
         <SimpleForm>
             <TextInput source="id" InputProps={{ disabled: true }} />
             <ReferenceInput source="userId" reference="users" />
             <TextInput source="title" />
-            <TextInput source="body" multiline rows={5} />
+            <EditButton/>
         </SimpleForm>
     </Edit>
 );
 
-export const PostCreate = () => (
-  <Create>
-    <SimpleForm>
-      <ReferenceInput source="userId" />
-      <TextInput source="title" />
-      <TextInput source="body" multiline rows={5} />
-    </SimpleForm>
-  </Create>
+
+export const AlbumCreate = () => (
+    <Create>
+        <SimpleForm>
+            <TextInput source="id" />
+            <ReferenceInput source="userId" reference="users" />
+            <TextInput source="title" />
+        </SimpleForm>
+    </Create>
 );
+
